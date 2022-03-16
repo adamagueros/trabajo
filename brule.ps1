2f29920a2d763f0f281540533e2c8317b9927280
adamagueros <70550229+adamagueros@users.noreply.github.com>
2022-03-16 16:10:20 -0500

    Add files via upload

---
+++ brule.ps1
@@ -0,0 +1,35 @@
+write-host ""
+write-host "               .,;,''.....',,,,,',,,;;:::lxkO00000OOOkkkkkxxxxxxxxxxxxxxxkkxxxkkkkxc...  ..............   " -foregroundcolor green
+write-host "               .,:;..'......',,,,,,,;;:cldkO0000000OOOkkkxxxxxxxxxxxxxxxkkxxxkkOOkkxc..  ..............   " -foregroundcolor green
+write-host "                .;:;''''....,;;;;;;;;::loxk00000000OOkkxxxxxxxxxxxxxxxxxxxxxkkOOOOOOkl. ......''''.....   " -foregroundcolor green
+write-host "                .,::;,,;;,,,;:::ccccccloddxOOO0000OOOkkkxxxxxxxxxxxxxxxxxkkkkkOO000OOk:.....''.''''.'.    " -foregroundcolor green
+write-host "                 .',,',,;;;,;;;:clooolodddxkkOO000OOkkkkxxxxxxdddxxxxxxxxxxxxkOOO00OOOd,....''...','..    " -foregroundcolor green
+write-host "                 .;c,..',,,,;;;:cloolooddxxkkkOOO0OOkkkkkxxxxxxxxxxxxxxxxkkkkkOOO000OOkl'......''','.     " -foregroundcolor green
+write-host "                 .',,,,,',,;;:;:cc:::cclxOOOOkkOOOOkkkkkkkkxxxxxxxxxxxxxxxkkkkkOOOO00OOd;'.....''','..    " -foregroundcolor green
+write-host "                 ..';;;;;;;,,,;;:;,,,;;:oxkkkkkkkkkxdddddddxxxxxxxxxxxxxxxxkkkOO000000Oxc''''.'''''''.    " -foregroundcolor green
+write-host "                  .',,;cc:;;,',;;;;;:cccldxdolcloodddlc::;:ccllllodxxxxddxxkkkkO00OOOOOkl'........',,.    " -foregroundcolor green
+write-host "                   ..';::;;::;;;;;;::cldxOOOkxolc:;:c:;;;;,,,,,;;:clddxxxdxxxdddxxxdxxkkl'.........'.     " -foregroundcolor green
+write-host "                   ..';:;;:cc::::::::cldkOOOkkxdl:;,''''',,,,,,''',:cllododolc:cloooodkOl,',,','','.      " -foregroundcolor green
+write-host "                 .','',;;,:c:;::cllcclloxkOOkkxollcccc:;;::::;,'.';llccll;::'',;:clooddxl;,,,,,'...       " -foregroundcolor green
+write-host "                 .'''..'''cccolclloolooodkkkkkkkkkOxddoooooollccclxkdodxd:;;,',;;,;clodxxl,,;,,'.         " -foregroundcolor green
+write-host "                 ..''...',lddoc:;coooooodxkkkkkkOOOkxdddddxxxxxdoddddddxxlclllodddlllodxxd;,;,'.          " -foregroundcolor green
+write-host "                 ..',,,,',lxo;,,,cddodddxkkkkkkkOOOOkdolooooolclc:ldddxxkxdxdodxkkxoodxxxl;,,...          " -foregroundcolor green
+write-host "                   ..'''..:xoc;,,lxddddxkOkkkkkkO00Oxddooolc:;;clokOkxxxkOddkxddxxxxxkkko;,''.            " -foregroundcolor green
+write-host "                   .''....'okxxxxxxkkOOOOOkkkkkkOOkddoooool,..:odxxxdoddkOxlcoxkkOOOkkdl:,..              " -foregroundcolor green
+write-host "                    .'''''.;xkO0OOOOO000OOkkkkkxxxdolllllcc'....';;;;;:coxkkdodkkkkkxxol;..               " -foregroundcolor green
+write-host "                    ...''''.cO00OxkOO00OOkkkxxxddoolccccccc:;,;col:;;,,,;coddxk00Okkkkd:,.                " -foregroundcolor green
+write-host "                     .''''..,okxocx0OOOOkkkkxxddollcccc:c:clodxkxdoddddxkkxoodkOOOOkkko'.                 " -foregroundcolor green
+write-host "                     ........'lxdcd0Okkkkkkkxxdoooollc::cldkkkxxxdddxxxkO0OxddxkOOOkkx;                   " -foregroundcolor green
+write-host "                        .....'lkkxkOOkkxxxkxxxdolooooc:::cc;,,,;clooodxkOOOOxxxkkOOkkd.                   " -foregroundcolor green
+write-host "                          .,;;lxOkOOOkkxxxxxxddlloddooc:cl;..   ....,coxkkkOkkkkkkkkko.                   " -foregroundcolor green
+write-host "                         .;lclllxOOOOkkxxxxxddolloddoolcoxxdl;.     .,:lxxxkkkkkkkkxdc.                   " -foregroundcolor green
+write-host "                        .:lllllclxO0Okkxxxxxddolloddollldddxkdl:;;;;coddddxxkkkkkkxd,                     " -foregroundcolor green
+write-host "                    ...,llllllccccdOOkkxxxxxddooloooolloddoodooooooodxxxxxxkkkkkkxx:                      " -foregroundcolor green
+write-host "                 .,;,.,cllllc:ccccldxxxxxxxxxddoolooooodddooooc;;;;:ldodxxxxkkkxxd:.                      " -foregroundcolor green
+write-host "                ,cc:;;:lccc:::::clccldxxxxxxxxddoooodooodoooool:;;:cooooddxxxxxxd;                        " -foregroundcolor green
+write-host "               ,cccccccc:;,'',,;;cc;,:lddddxddddooloddddddololc::;::cloodddxxxxd;                         " -foregroundcolor green
+write-host "           ..,:ccclcc::::;,'''',,,::,',:lodddddoolllodxxxddoooollcccloddxddxxxxc.                         " -foregroundcolor green
+write-host "        .,:cccccccccc:::::;,''''',,,;,..,:loooooollloodxxxddxddddoooddxxxxxxxxo.                          " -foregroundcolor green
+write-host "      ':ccc:::::::::::::c:::;,,'''''','....;ccllllllllooddddddddddoddxxxkxxxxd:.                          " -foregroundcolor green
+write-host "    ':cc:::::::::::::::cccccc::;,,'''''''.  .,;:cccllccclllllllllllodddxxxdxdol:,.. ':.                   " -foregroundcolor green
+write-host "  .;c::::::::::::::::::ccccccccc:;,'''''''.   .';;::c:;,;;;;;;:::::looodddddollccl:,dXo                   " -foregroundcolor green
