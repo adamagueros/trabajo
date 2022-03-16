9cbed35c77122fbc5a622975497525feb8cc0528
Jarod Abel <jarod@dronedeploy.com>
2022-03-16 10:27:09 -0500

    example

--- scripts/pwup.ps1
+++ scripts/pwup.ps1
@@ -1,19 +1,4 @@
-function question {
-    write-host "SELECT INPUT METHOD" -ForegroundColor cyan
-    write-host "A) List" -foregroundcolor yellow
-    write-host "B) File" -foregroundcolor yellow
-    $answer = read-host "CHOICE"
-    Switch ($answer) {
-        A { list }
-        B { file }
-        default {
-            Clear-Host
-            question
-        }
-    }
-}
-
-
+Import-Module "$PSScriptRoot\..\common\common.ps1"
 function list {
     Write-host "ENTER MACHINE NAMES:" -ForegroundColor cyan
     $global:computer = @()
